bad1c5e837e26a789b01d87b6973eb655b86b294
Anand Natu <natu.anand@gmail.com>
2026-02-16 13:02:59 -0500

    Added fusion module + diffusion head

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # miniVLA
 Small-scale proof of concept vision-language-action (VLA) model based on Keivalya Pandya's tutorial
+
+Credit: https://medium.com/@keivalyap/building-vla-models-from-scratch-ii-0180020dbc85
---
+++ models/fusion.py
@@ -0,0 +1,28 @@
+
+"""Fusion module: Combines the different
+encodings from our various modalities (image, text, state).
+More generally this module also decides HOW information is blended
+across the different modalities but the implementation here
+is trivial (concatenation)"""
+
+import torch
+import torch.nn as nn
+
+class FusionMLP(nn.Module):
+    def __init__(self, d_model=128):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(3 * d_model, d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, d_model),
+        )
+        self.ln = nn.LayerNorm(d_model)
+
+    def forward(self, img_token, txt_token, state_token):
+        # Concatenate the tokens along the last dimension
+        x = torch.cat([img_token, txt_token, state_token], dim=-1)
+        # Pass through MLP (Multi-Layer Perceptron)
+        x = self.net(x)
+        # Layer Normalization
+        x = self.ln(x)
+        return x
